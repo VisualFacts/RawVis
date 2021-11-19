0a97d653ee546a650ac373c29fb2f2cbefd2ea16
stavmars <stavrosmrl@gmail.com>
2021-11-19 15:51:21 +0200

    Change base context path for production

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -116,7 +116,7 @@ module.exports = options => ({
       template: './src/main/webapp/index.html',
       chunksSortMode: 'auto',
       inject: 'body',
-      base: '/',
+      base: options.env === 'production' ? '/platform/' : '',
     }),
   ]
 });
