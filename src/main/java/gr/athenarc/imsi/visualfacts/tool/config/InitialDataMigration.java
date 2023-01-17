da92395bb4ea8c0a5a7585b6e95dcf987b780545
stavr <stavrosmrl@gmail.com>
2023-01-17 11:01:32 +0200

    Skip initial data migration

--- src/main/java/gr/athenarc/imsi/visualfacts/tool/config/InitialDataMigration.java
+++ src/main/java/gr/athenarc/imsi/visualfacts/tool/config/InitialDataMigration.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
 
 @Configuration
 public class InitialDataMigration {
@@ -17,7 +14,7 @@ public class InitialDataMigration {
 
     public InitialDataMigration(ApplicationProperties applicationProperties) throws IOException {
         this.applicationProperties = applicationProperties;
-        String initialDataPath = getClass().getClassLoader().getResource("initial-data").getPath();
+        /*String initialDataPath = getClass().getClassLoader().getResource("initial-data").getPath();
 
         File workspaceDirectory = new File(applicationProperties.getWorkspacePath());
         if (! workspaceDirectory.exists()){
@@ -31,6 +28,6 @@ public class InitialDataMigration {
             } catch (FileAlreadyExistsException e) {
                 log.debug("Metadata file " + file.getName() + " already exists in workspace directory.");
             }
-        }
+        }*/
     }
 }
